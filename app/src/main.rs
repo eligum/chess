5b8a090b58031eb5ff489b1de25395e1bf426c92
eligum <eligum.djo@gmail.com>
2024-03-05 11:33:27 +0100

    wip 2

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,14 +4,14 @@ const BOARD_SIZE: Vec2 = Vec2::new(800.0, 800.0);
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.6, 0.4, 0.3)))
+        .insert_resource(ClearColor(Color::BEIGE))
         .add_plugins(
             DefaultPlugins
-                .set(ImagePlugin::default_nearest())
+                .set(ImagePlugin::default_linear())
                 .set(WindowPlugin {
                     primary_window: Some(Window {
                         title: "Chess engine".into(),
-                        resolution: (1000.0, 1000.0).into(),
+                        resolution: (900.0, 900.0).into(),
                         resizable: false,
                         ..default()
                     }),
