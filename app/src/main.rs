36b971e981879e2bcec61e1a921ab8eef00cfe28
eligum <eligum.djo@gmail.com>
2024-03-05 08:10:43 +0100

    wip

--- app/src/main.rs
+++ app/src/main.rs
@@ -4,8 +4,21 @@ const BOARD_SIZE: Vec2 = Vec2::new(800.0, 800.0);
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .insert_resource(ClearColor(Color::rgb(0.1, 0.1, 0.1)))
+        .insert_resource(ClearColor(Color::rgb(0.6, 0.4, 0.3)))
+        .add_plugins(
+            DefaultPlugins
+                .set(ImagePlugin::default_nearest())
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Chess engine".into(),
+                        resolution: (1000.0, 1000.0).into(),
+                        resizable: false,
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .build(),
+        )
         .add_systems(Startup, setup)
         .run();
 }
@@ -13,7 +26,7 @@ fn main() {
 #[derive(Component)]
 struct Board;
 
-fn setup(mut commands: Commands) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let LIGHT_SQUARES_COLOR = Color::hex("#FFD180").unwrap();
     let DARK_SQUARES_COLOR = Color::hex("#795548").unwrap();
 
@@ -21,15 +34,18 @@ fn setup(mut commands: Commands) {
     commands.spawn(Camera2dBundle::default());
 
     // Board
+    let texture = asset_server.load("chess_pieces.png");
+
     commands.spawn((
         SpriteBundle {
             transform: Transform {
                 translation: Vec3::new(0.0, 0.0, 0.0),
                 ..default()
             },
+            texture,
             sprite: Sprite {
-                color: LIGHT_SQUARES_COLOR,
-                custom_size: Some(BOARD_SIZE),
+                // color: LIGHT_SQUARES_COLOR,
+                // custom_size: Some(BOARD_SIZE),
                 ..default()
             },
             ..default()
